d5848d4ceacb1d9092d2dfacd972b279914ba54a
Ngoni <ngonimapfumo@hotmail.com>
2024-02-06 08:06:25 +0200

    Update 103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 # 103-magic_calculation.py
-# Ekun-Thomas Francis<ekun_tee@yahoo.com>
 """Define a MagicClass matching exactly a bytecode provided by Holberton."""
 
 import math
